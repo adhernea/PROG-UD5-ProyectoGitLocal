fdc0878422ea2d8092463425d7299080e54402e5
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2025-04-29 10:20:28 +0100

    feat(App.java): Display menu options

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Menú:");
+        System.out.println("- Opción 1: Añadir persona.");
+        System.out.println("- Opción 2: Borrar persona.");
+        System.out.println("- Opción 3: Salir.");
     }
 }
